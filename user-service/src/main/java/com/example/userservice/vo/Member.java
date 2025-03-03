15f0c12ef869c170edc800aab29f4dd4caf0edb2
SmS <mongddangand@likelion.org>
2025-03-04 01:22:57 +0900

    [h2 연동]
    - h2 연동

---
+++ user-service/src/main/java/com/example/userservice/vo/Member.java
@@ -0,0 +1,12 @@
+package com.example.userservice.vo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+}

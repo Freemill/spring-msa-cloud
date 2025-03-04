2eac49214ae496f53f4787f3cf34b44402c41adc
SmS <mongddangand@likelion.org>
2025-03-04 12:14:26 +0900

    [SECURITY]
    - Security 설정

---
+++ user-service/src/main/java/com/example/userservice/security/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.userservice.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(new AntPathRequestMatcher("/users/**", "POST")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/health_check", "GET")).permitAll()
+                )
+                .headers((header) -> header.frameOptions((frameOption) -> frameOption.disable()))
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
